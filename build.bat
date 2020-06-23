174f40f29c047898813c3828436b360322aa6f83
Mitter <michael.mitter@imba.oeaw.ac.at>
2020-06-23 18:10:29 +0200

    add icon to build script

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --hidden-import xldr --hidden-import pkg_resources.py2_warn --noconsole --onedir --noconfirm .\gui.py
+pyinstaller --hidden-import xldr --hidden-import pkg_resources.py2_warn --noconsole --onedir --noconfirm --name FGO-Scheduler --add-data data;data .\gui.py
